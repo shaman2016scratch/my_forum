3e844f3bf74a4ceb778ba72d188510bb23638c04
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-28 21:54:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ Doc = {}
 Doc.lang = document.getEletemtByID('lang')
 const url = new URL(window.location)
 const sp = url.searchParams
+if (sp.get('lang')) {
+  Doc.lang.value = sp.get('lang')
+} else {
+  sp.set('lang', Doc.lang.value)
+}
